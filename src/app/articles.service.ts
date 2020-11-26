7983068e5ce5329fe24a22e0900cc575cf215dc2
zubr <zubr@rambler.ru>
2020-11-26 23:11:04 +0300

    progress bar

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
-import {Observable, Subject, throwError} from 'rxjs';
+import {concat, interval, Observable, Subject, throwError} from 'rxjs';
 
 import { environment } from '../environments/environment';
-import {catchError} from 'rxjs/operators';
+import {catchError, map, take, takeLast, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
   public error$: Subject<string> = new Subject<string>();
+  public progress$: Subject<string> = new Subject<string>();
 
   constructor(
     private http: HttpClient
@@ -17,11 +18,28 @@ export class ArticlesService {
 
   getArticles(context: string): Observable<any> {
     const options = {params: new HttpParams().set('page-size', '50')};
-    return this.http.get(
+
+    const total = 20;
+
+    // Fake progress
+    // progress$ is needed to demonstrate progress because the request is very fast
+    const progress$ = interval(100).pipe(
+      take(total),
+      map(num => num / total * 100),
+      tap(percent => this.progress$.next(`${percent}%`))
+    );
+
+    const request =  this.http.get(
       `https://content.guardianapis.com/search?q=${context}&api-key=${environment.apiKey}`, options
     ).pipe(
       catchError(this.handleError.bind(this))
     );
+
+    return concat(progress$, request)
+      .pipe(
+        takeLast(1),
+        tap(() => this.progress$.next('0'))
+      );
   }
 
   private handleError(error: HttpErrorResponse) {
